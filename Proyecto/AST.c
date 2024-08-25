aea6a25dc38e8c963a9259b31fb198e408ee1b69
Uriel-Gz <uriguzman150212@gmail.com>
2024-08-25 09:28:16 -0300

    reubicacion de main en main.c

--- Proyecto/AST.c
+++ Proyecto/AST.c
@@ -30,27 +30,3 @@ void showTree(AST tree) {
         }
     }
 }
-
-int main(){
-    char *num = "num";
-    char *sum = "sum";
-    //2+2
-    
-    //tabla de simbolos
-    Install(num,1,1);
-    Install(sum,1,1);
-     
-    //Busqueda en la tabla
-    struct Tsymbol *valor = Lookup(num);
-    struct Tsymbol *operador = Lookup(sum);
-
-
-    //arbol
-    AST der = createTree(30,valor->type,valor->name,NULL,NULL);
-    AST izq = createTree(20,valor->type,valor->name,NULL,NULL);
-    showTree(createTree(1,operador->type,operador->name,&izq,&der));
-    
-    //limpio table
-    DeleteList();
-    return 0;
-}
--- Proyecto/main.c
+++ Proyecto/main.c
@@ -1,10 +1,26 @@
 
 #include "AST.h"
 
-int main(int argc, char const *argv[])
-{
-    AST ar = createTree(1, 1, "raiz", NULL, NULL);
+int main(){
+    char *num = "num";
+    char *sum = "sum";
+    //2+2
+    
+    //tabla de simbolos
+    Install(num,1,1);
+    Install(sum,1,1);
+     
+    //Busqueda en la tabla
+    struct Tsymbol *valor = Lookup(num);
+    struct Tsymbol *operador = Lookup(sum);
 
-    showTree(ar);
+
+    //arbol
+    AST der = createTree(30,valor->type,valor->name,NULL,NULL);
+    AST izq = createTree(20,valor->type,valor->name,NULL,NULL);
+    showTree(createTree(1,operador->type,operador->name,&izq,&der));
+    
+    //limpio table
+    DeleteList();
     return 0;
 }
