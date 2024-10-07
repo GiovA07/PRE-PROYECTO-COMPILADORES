7e78acb78bce03805343957c384a6b9be6bbde42
Uriel-Gz <uriguzman150212@gmail.com>
2024-10-07 19:10:01 -0300

    funciones de pseudo asembler

--- Proyecto/include/PseudoASM.h
+++ Proyecto/include/PseudoASM.h
@@ -1,21 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <symbol.h>
 #include "AST.h"
 
 #ifndef PSEUDOASM_H
 #define PSEUDOASM_H
 
 enum ASM_TAG {
-    ASIGN,
-    IF, WHILE,
-    OR, NOT, AND,
-    IGUAL, MAYOR, MENOR,
-    SUMA, RESTA, RESTO, PRODUCTO, DIVISION
+    T_ASIGN,
+    T_IF, T_WHILE,
+    T_OR, T_NOT, T_AND,
+    T_IGUAL, T_MAYOR, T_MENOR,
+    T_SUM, T_RES, T_MOD, T_PROD, T_DIV
 };
 
 typedef struct PseudoASM {
-    ASM_TAG tag;
+    enum ASM_TAG tag;
     Tsymbol* op1;
     Tsymbol* op2;
     Tsymbol* result;
@@ -23,8 +22,11 @@ typedef struct PseudoASM {
 } PseudoASM;
 
 
-PseudoASM* instructions;
+PseudoASM* instructions = NULL;
 
-PseudoASM* traslate(enum ASM_TAG tag, AST* op1, AST* op2);
+
+void deleteInstructions();
+void generateCode(AST* ar);
+PseudoASM* traslate(enum TYPES tag, AST* op1, AST* op2, AST* res);
 
 #endif
--- Proyecto/src/PseudoASM.c
+++ Proyecto/src/PseudoASM.c
@@ -1,61 +1,99 @@
 #include "../include/PseudoASM.h"
 
 
-PseudoASM* traslate(enum ASM_TAG tag, AST* op1, AST* op2) {
+PseudoASM* traslate(enum TYPES tag, AST* op1, AST* op2, AST* res) {
 
     PseudoASM* sequense = (PseudoASM*)malloc(sizeof(PseudoASM));
 
-    sequense->tag = tag;
     sequense->op1 = op1->symbol;
     sequense->op1 = op2->symbol;
+    sequense->result = res->symbol;
 
-    if (tag == ASIGN) {
+    if (tag == ASIG) {
+        sequense->tag = T_ASIGN;
         sequense->result->value = op1->symbol->value;
     } else if (tag == SUMA) {
+        sequense->tag = T_SUM;
         sequense->result->value = op1->symbol->value + op2->symbol->value;
     } else if (tag == RESTA) {
+        sequense->tag = T_RES;
         sequense->result->value = op1->symbol->value - op2->symbol->value;
-    } else if (tag == PRODUCTO) {
+    } else if (tag == PROD) {
+        sequense->tag = T_PROD;
         sequense->result->value = op1->symbol->value * op2->symbol->value;
-    } else if (tag == DIVISION) {
+    } else if (tag == EDIV) {
+        sequense->tag = T_DIV;
         if (op2->symbol->value != 0)
             sequense->result->value = op1->symbol->value / op2->symbol->value;
         else
             printf("Error en la division, division por cero no permitido");
-    } else if (tag == RESTO) {
+    } else if (tag == ERESTO) {
+        sequense->tag = T_MOD;
         sequense->result->value = op1->symbol->value % op2->symbol->value;
-    } else if (tag == NOT) {
+    } else if (tag == ENOT) {
+        sequense->tag = T_NOT;
         if (op1->symbol->value == 0)
             sequense->result->value = 1;
         else
             sequense->result->value = 0;
-    } else if (tag == AND) {
+    } else if (tag == EAND) {
+        sequense->tag = T_AND;
         if (op1->symbol->value == op2->symbol->value && op1->symbol->value == 1)
             sequense->result->value = 1;
         else
             sequense->result->value = 0;
-    } else if (tag == OR) {
+    } else if (tag == EOR) {
+        sequense->tag = T_OR;
         if (1 == op2->symbol->value || op1->symbol->value == 1)
             sequense->result->value = 1;
         else
             sequense->result->value = 0;
-    } else if (tag == MAYOR) {
+    } else if (tag == EMAYORQUE) {
+        sequense->tag = T_MAYOR;
         sequense->result->value = op1->symbol->value > op2->symbol->value? 1 : 0;
-    } else if (tag == MENOR) {
+    } else if (tag == EMENORQUE) {
+        sequense->tag = T_MENOR;
         sequense->result->value = op1->symbol->value < op2->symbol->value? 1 : 0;
-    } else if (tag == MAYOR) {
+    } else if (tag == EEQ) {
+        sequense->tag = T_IGUAL;
         sequense->result->value = op1->symbol->value == op2->symbol->value? 1 : 0;
-    } else if (tag == IF) {
+    } else if (tag == EIF) {
+        sequense->tag = T_IF;
         if (op1->symbol->value == 1)
             sequense->result = op2->left->symbol;
         else
             sequense->result = op2->right->symbol;
-    } else {
+    } else if (tag == EWHILE) {
+        sequense->tag = T_WHILE;
         if (op1->symbol->value == 1)
             sequense->result = op2;
         else
             sequense->result = NULL;
+    } else {
+        return instructions;
     }
     
+    
     return sequense;
+}
+
+void generateCode(AST* ar) {
+
+    PseudoASM* subSequense = traslate(ar->left->symbol->type, ar->left->left, ar->left->right, ar->left);
+    subSequense->next = traslate(ar->right->symbol->type, ar->right->left, ar->right->right, ar->right);
+
+    subSequense->next = traslate(ar->symbol->type, ar->left, ar->right, ar);
+
+    instructions = subSequense;
+}
+
+void deleteInstructions() {
+    PseudoASM* current = instructions;
+    PseudoASM* next = NULL;
+
+    while (current != NULL) {
+        next = current->next;
+        free(current);
+        current = next;
+    }
 }
